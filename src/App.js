4495c1cecdaa480dfe9a96a1a0dfc6c8ea580af9
David <davida@ambrosebuilding.com.au>
2021-07-25 20:10:27 +1000

    Initial commit hype hype

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
+import { cartActions } from "./store/cart-slice";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const isCartVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.cart.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.state}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {isCartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
